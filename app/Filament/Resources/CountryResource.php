575d08c4d51c7099ec3f87b9060c22f5fb3398fd
Anjas <anjasopo@gmail.com>
2024-01-10 00:11:03 +0700

    Update: filament resource, form

---
+++ app/Filament/Resources/CountryResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CountryResource\Pages;
+use App\Filament\Resources\CountryResource\RelationManagers;
+use App\Models\Country;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CountryResource extends Resource
+{
+    protected static ?string $model = Country::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-flag';
+
+    // Mengganti nama label
+    protected static ?string $navigationLabel = 'Country';
+
+    // Mengganti nama diatas title
+    protected static ?string $modelLabel = 'Employees Country';
+
+    // Membuat group management dan memindahkan Country ke dalamnya
+    protected static ?string $navigationGroup = 'System Management';
+
+    // Mengurutkan dalam navgroup
+    protected static ?int $navigationSort = 1;
+
+    // Jika ingin mengganti link ke /employees-countries
+    // protected static ?string $slug = 'employees-countries';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCountries::route('/'),
+            'create' => Pages\CreateCountry::route('/create'),
+            'edit' => Pages\EditCountry::route('/{record}/edit'),
+        ];
+    }
+}
